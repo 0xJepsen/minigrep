7d56c90182f0eb39b28a181e6038e49d6473ec99
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-06-06 08:18:35 -0600

    searching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use minigrep::run;
+use minigrep::Config;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
+    }
 }
